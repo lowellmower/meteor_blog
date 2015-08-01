c6adf1e596e5b4ca2435557fee1b72aaa3c4a487
Lowell Mower <lowell.m.mower@gmail.com>
2015-08-01 14:18:03 -0400

    add insert method for post

--- client/blog.js
+++ client/blog.js
@@ -2,3 +2,25 @@ Blog.config({
   syntaxHighlighting: true,
   syntaxHighlightingTheme: 'github'
 });
+
+Template.new.events({
+  'submit .new-post': function(event){
+    var title = event.target.title.value;
+    var body = event.target.body.value;
+    Meteor.call("addPost", title, body);
+    event.target.title.value = "";
+    event.target.body.value = "";
+    return false;
+  }
+});
+
+Meteor.methods({
+  addPost: function(title, body){
+    Posts.insert({
+      title: title,
+      body: body,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+    });
+  }
+});
